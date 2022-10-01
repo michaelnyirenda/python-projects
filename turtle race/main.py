4b7cf6a7e8e99630fe2dad3fc075e509ec30c1ac
Michael Nyirenda <michael00nyirenda@gmail.com>
2022-10-01 13:52:37 +0200

    day 19 turtle race start

---
+++ turtle race/main.py
@@ -0,0 +1,24 @@
+from turtle import Turtle, Screen
+
+raph =  Turtle()
+don = Turtle()
+mikey = Turtle()
+leo = Turtle()
+
+screen = Screen()
+screen.setup(width = 500, height = 500)
+user_bet = screen.textinput(title = "make your bet", prompt = "which turtle will win the race? enter a name: ")
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
