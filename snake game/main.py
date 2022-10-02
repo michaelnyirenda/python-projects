395f4757d4f36d5390c5af149427e0f9775d24e1
Michael Nyirenda <michael00nyirenda@gmail.com>
2022-10-02 18:45:00 +0200

    controlling the snake with keypresses

--- snake game/main.py
+++ snake game/main.py
@@ -1,7 +1,8 @@
-from turtle import Screen
+from turtle import Screen, down
 from snake import Snake
 import time
 
+# screen setup
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
@@ -9,6 +10,15 @@ screen.title("Danger Noodle: The Game")
 screen.tracer(0)
 
 snake = Snake()
+
+# screen listening for key presses
+screen.listen()
+
+# key press handling
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
 game_running = True
 while game_running:
--- snake game/snake.py
+++ snake game/snake.py
@@ -2,7 +2,7 @@ from turtle import Turtle
 
 SEGMENT_POS = [(0,0), (-20, 0), (-40, 0)] # list of segment positions
 MOVE_DISTANCE = 20 # distance to move each segment
-
+DIRECTIONS = [0, 90, 180, 270] # Right, Up, Left, Down
 class Snake:
     def __init__(self):
         self.segments =[]
@@ -23,5 +23,21 @@ class Snake:
             new_x = self.segments[i-1].xcor() # the x coordinate of the segment in front of it
             new_y = self.segments[i-1].ycor() # the y coordinate of the segment in front of it
             self.segments[i].goto(new_x, new_y)
-        
         self.segments[0].forward(MOVE_DISTANCE)
+        
+    def up(self):
+        if self.segments[0].heading() != DIRECTIONS[3]: # if not heading down
+            self.segments[0].setheading(DIRECTIONS[1]) # then head up
+    
+    def down(self):
+        if self.segments[0].heading() != DIRECTIONS[1]: # if not heading up
+            self.segments[0].setheading(DIRECTIONS[3]) # then head down
+        
+    def left(self):
+        if self.segments[0].heading() != DIRECTIONS[0]: # if not heading right
+            self.segments[0].setheading(DIRECTIONS[2]) # then head left
+        
+    def right(self):
+        if self.segments[0].heading() != DIRECTIONS[2]: # if not heading left
+            self.segments[0].setheading(DIRECTIONS[0]) # then head right
+        
