12d9cb8d74be80d6805af953613d384fee2e625f
Michael Nyirenda <michael00nyirenda@gmail.com>
2022-10-28 19:45:07 +0200

    paddle responds to keypresses

--- pong/main.py
+++ pong/main.py
@@ -1,17 +1,43 @@
 # the screen that the pong game will happen in
-
+# from paddle import Paddle
 from turtle import *
+X = 350
+Y = 0
 
 screen = Screen()
 screen.bgcolor("black")
-screen.screensize(800, 600)
+screen.setup(800, 600)
+screen.tracer(0)
+
+# the paddle that will move up and down
+paddle = Turtle(shape = "square")
+paddle.speed(0)
+paddle.penup()
+paddle.color("white")
+paddle.shapesize(stretch_wid=5, stretch_len=1)
+paddle.setposition(X, Y)
 
 
+def up():
+    #change the ycor by 20 pixels
+    new_y = paddle.ycor() + 20
+    paddle.goto(paddle.xcor(), new_y)
+   
+  
+def down():
+    #move paddle by 20 pixels down
+    new_y = paddle.ycor() - 20
+    paddle.goto(paddle.xcor(), new_y)
+  
+# key press handling
+screen.listen()
+screen.onkey(up, "Up")
+screen.onkey(down, "Down")
 
 
-
-
-
+game_on = True
+while game_on:
+    screen.update()
 
 
 
--- pong/paddle.py
+++ pong/paddle.py
@@ -1,1 +1,23 @@
-# a paddle will move up and down until it reaches the top or bottom of the screen 
+# # a paddle will move up and down until it reaches the top or bottom of the screen 
+# from turtle import *
+# x = 350
+# y = 0
+# class Paddle:
+#     def __init__(self):
+        
+#         paddle = Turtle(shape = "square")
+#         paddle.speed(0)
+#         paddle.penup()
+#         paddle.color("white")
+#         paddle.shapesize(stretch_wid=5, stretch_len=1)
+#         paddle.setposition(x, y)
+        
+#     def up():
+#         #change the ycor by 20 pixels
+#         new_y = paddle.sety(y + 20)
+#         paddle.goto(x, new_y)
+        
+#     def down(paddle):
+#         #move paddle by 20 pixels down
+#         new_y = paddle.sety(y - 20)
+#         paddle.goto(x, new_y)
