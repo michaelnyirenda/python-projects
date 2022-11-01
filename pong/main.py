31a989e2a7fc94767f586065f7f8f5c21dc9dbad
Michael Nyirenda <michael00nyirenda@gmail.com>
2022-11-01 17:55:29 +0200

    create ball

--- pong/main.py
+++ pong/main.py
@@ -1,44 +1,31 @@
 # the screen that the pong game will happen in
-# from paddle import Paddle
+from paddle import Paddle
+from ball import Ball
 from turtle import *
-X = 350
-Y = 0
+import time
 
 screen = Screen()
 screen.bgcolor("black")
 screen.setup(800, 600)
 screen.tracer(0)
 
-# the paddle that will move up and down
-paddle = Turtle(shape = "square")
-paddle.speed(0)
-paddle.penup()
-paddle.color("white")
-paddle.shapesize(stretch_wid=5, stretch_len=1)
-paddle.setposition(X, Y)
 
+right_paddle = Paddle(350, 0)
+left_paddle = Paddle(-350, 0)
+ball = Ball()
 
-def up():
-    #change the ycor by 20 pixels
-    new_y = paddle.ycor() + 20
-    paddle.goto(paddle.xcor(), new_y)
-   
-  
-def down():
-    #move paddle by 20 pixels down
-    new_y = paddle.ycor() - 20
-    paddle.goto(paddle.xcor(), new_y)
-  
 # key press handling
 screen.listen()
-screen.onkey(up, "Up")
-screen.onkey(down, "Down")
-
+screen.onkey(right_paddle.up, "Up")
+screen.onkey(right_paddle.down, "Down")
+screen.onkey(left_paddle.up, "w")
+screen.onkey(left_paddle.down, "s")
 
 game_on = True
 while game_on:
+    time.sleep(0.1)
     screen.update()
-
+    ball.move()
 
 
 
