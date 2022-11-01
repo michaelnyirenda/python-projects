c8a4995cddc68b43b149b5f23e8b648b27743e19
Michael Nyirenda <michael00nyirenda@gmail.com>
2022-11-01 21:14:01 +0200

    keeping track of score for both players

--- pong/main.py
+++ pong/main.py
@@ -2,6 +2,7 @@
 from paddle import Paddle
 from ball import Ball
 from turtle import *
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -9,6 +10,7 @@ screen.bgcolor("black")
 screen.setup(800, 600)
 screen.tracer(0)
 
+score = Scoreboard()
 
 right_paddle = Paddle(350, 0)
 left_paddle = Paddle(-350, 0)
@@ -38,10 +40,17 @@ while game_on:
         ball.bounce_x()
     
     
-    
-    if ball.xcor() > 380 or ball.xcor() < -380:
+    if ball.xcor() > 380:
         ball.reset_position()
-    # add point to scoreboard
+        # add point to scoreboard
+        score.l_point()
+        
+        
+    if ball.xcor() < -380:
+        ball.reset_position()
+        # add point to scoreboard
+        score.r_point()
+    
    
     
 
