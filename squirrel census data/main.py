994461c8ae183a363ed46bfead33ea2bf19972da
Michael Nyirenda <michael00nyirenda@gmail.com>
2023-01-27 14:37:14 +0200

    create fur count csv

---
+++ squirrel census data/main.py
@@ -0,0 +1,12 @@
+import pandas as pd
+data = pd.read_csv("2018_central_park_squirrel_census.csv")
+
+black_count = len(data[data["Primary Fur Color"] == "Black"])
+grey_count = len(data[data["Primary Fur Color"] == "Gray"])
+cinnamon_count = len(data[data["Primary Fur Color"] == "Cinnamon"])
+
+fur_dict = {'fur': ['black', 'grey', 'cinnamon'],
+             'count': [black_count, grey_count, cinnamon_count]}
+
+fur_df = pd.DataFrame(fur_dict)
+fur_df.to_csv("fur_count.csv")
