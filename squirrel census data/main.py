3e3a42e883d7cdbc959a0ed507dee81a72ff1e4d
Michael Nyirenda <michael00nyirenda@gmail.com>
2023-01-27 14:49:31 +0200

    color to colour

--- squirrel census data/main.py
+++ squirrel census data/main.py
@@ -5,7 +5,7 @@ black_count = len(data[data["Primary Fur Color"] == "Black"])
 grey_count = len(data[data["Primary Fur Color"] == "Gray"])
 cinnamon_count = len(data[data["Primary Fur Color"] == "Cinnamon"])
 
-fur_dict = {'fur': ['black', 'grey', 'cinnamon'],
+fur_dict = {'fur colour': ['black', 'grey', 'cinnamon'],
              'count': [black_count, grey_count, cinnamon_count]}
 
 fur_df = pd.DataFrame(fur_dict)
