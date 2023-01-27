02ed9b53c393379cfdb2c963177c3b78352ca670
Michael Nyirenda <michael00nyirenda@gmail.com>
2023-01-27 13:38:39 +0200

    dealing with rows/columns

--- weather data/main.py
+++ weather data/main.py
@@ -10,6 +10,28 @@
 #     print(temperature)    
 
 import pandas 
+import numpy as np
 
 data = pandas.read_csv("weather_data.csv")
-print(data)
+# print(type(data))
+# print(type(data["temp"]))
+# data_dict = data.to_dict()
+# print(data_dict)
+
+# temp_list = data["temp"].to_list()
+# temp_average = np.mean(temp_list)
+# print(temp_average)
+
+# temp_average = data["temp"].mean()
+# print(temp_average)
+
+# print(data["temp"].max())
+
+# print(data[data.temp == data["temp"].max()])
+
+monday = data[data.day == "Monday"]
+monday_temp_C = int(monday.temp)
+monday_temp_F = monday_temp_C * 9/5 + 32
+print(monday_temp_F)
+
+
