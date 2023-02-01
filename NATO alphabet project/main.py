6aadaaf76d2c5c337991ed1f4f4965c43159ba58
Michael Nyirenda <michael00nyirenda@gmail.com>
2023-02-01 13:06:48 +0200

    more practice

--- NATO alphabet project/main.py
+++ NATO alphabet project/main.py
@@ -5,3 +5,14 @@ print(new_numbers)
 name = "michael"
 letter_list = [letter for letter in name]
 print(letter_list)
+
+new_range = [n*2 for n in range(1, 5)] # [1, 2, 3, 4]
+print(new_range)
+
+names = ["Katara", "Toph", "Aang", "Zuko", "Sokka"]
+short_names = [name for name in names if len(name)<5]
+print(short_names)
+
+names = ["Katara", "Toph", "Aang", "Zuko", "Sokka"]
+upper_names = [name.upper() for name in names if len(name)>4]
+print(upper_names)
