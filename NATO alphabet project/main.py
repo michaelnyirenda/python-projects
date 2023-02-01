07a8a5c9ec800de14a549bae342fd337b88ec78a
Michael Nyirenda <michael00nyirenda@gmail.com>
2023-02-01 16:51:09 +0200

    optimizing nato alphabet project

--- NATO alphabet project/main.py
+++ NATO alphabet project/main.py
@@ -3,8 +3,7 @@ import pandas as p
 #TODO 1. Create a dictionary in this format:
 # {"A": "Alfa", "B": "Bravo"}
 data = p.read_csv("nato_phonetic_alphabet.csv")
-nato = p.DataFrame(data)
-nato_dict = {row.letter:row.code for (index, row) in nato.iterrows()}
+nato_dict = {row.letter:row.code for (index, row) in data.iterrows()}
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 word = input("Enter a word: ").upper()
