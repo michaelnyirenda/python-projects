05ebc99dbc7c32621ceff598da2eabc4cb401cb3
Michael Nyirenda <michael00nyirenda@gmail.com>
2023-02-03 21:44:29 +0200

    learning oython arguments

---
+++ mile to km converter/playground.py
@@ -0,0 +1,7 @@
+def add(*args):
+    total = 0
+    for arg in args:
+        total += arg
+    return total
+
+print(add(1,2,3))
--- mile to km converter/practice.py
+++ mile to km converter/practice.py
@@ -9,7 +9,7 @@ window.minsize(width=500, height=300)
 # Label
 
 my_label = tkinter.Label(text="I am a label", font=("Arial", 24, "italic"))
-my_label.pack(expand = True) # pack() is used to show the label on the screen
+my_label.pack(side = "left") # pack() is used to show the label on the screen
 
 
 
