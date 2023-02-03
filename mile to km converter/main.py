ca5c9a4367833d3be14258fd959cf64b1c384553
Michael Nyirenda <michael00nyirenda@gmail.com>
2023-02-04 01:32:08 +0200

    mile to km converter complete

---
+++ mile to km converter/main.py
@@ -0,0 +1,51 @@
+from tkinter import *
+
+
+def button_clicked():
+    result.config(text=round(int(input.get())*1.609344)) # get() is used to get the text from the entry
+
+# Window
+window = Tk()
+window.title("Mile to Km Converter")
+window.config(padx=10, pady=10) # padding
+ 
+# Label
+miles = Label(text="Miles", font=("Arial"))
+miles.grid(column= 2, row = 0) # pack() is used to show the label on the screen
+miles.config(padx=10, pady=10)
+
+equal = Label(text="is equal to", font=("Arial"))
+equal.grid(column= 0, row = 1) # pack() is used to show the label on the screen
+equal.config(padx=10, pady=10)
+
+km = Label(text="Km", font=("Arial"))
+km.grid(column= 2, row = 1) # pack() is used to show the label on the screen
+km.config(padx=10, pady=10)
+
+result = Label(text="0", font=("Arial"))
+result.grid(column= 1, row = 1) # pack() is used to show the label on the screen
+result.config(padx=10, pady=10)
+
+# Button
+button = Button(text="Calculate", command = button_clicked) # button is an object
+button.grid(column = 1,  row = 2) # pack() is used to show the button on the screen
+button.config(padx=10, pady=10)
+
+# Entry
+input = Entry(width=10)
+input.grid(column = 1,  row = 0) # pack() is used to show the entry on the screen
+
+window.mainloop()
+
+
+
+
+
+
+
+
+
+
+
+window.mainloop()
+
--- mile to km converter/practice.py
+++ mile to km converter/practice.py
@@ -1,30 +1,30 @@
 from tkinter import *
 
-window = Tk()
-
-window.title("My first GUI program")
-
-window.minsize(width=500, height=300)
- 
-# Label
-
-my_label = Label(text="I am a label", font=("Arial", 24, "italic"))
-my_label.pack() # pack() is used to show the label on the screen
-
-
-# Button
 
 def button_clicked():
     my_label.config(text=input.get()) # get() is used to get the text from the entry
 
+# Window
+window = Tk()
+window.title("My first GUI program")
+window.minsize(width=500, height=300)
+window.config(padx=20, pady=20) # padding
+ 
+# Label
+my_label = Label(text="I am a label", font=("Arial", 24, "italic"))
+my_label.grid(column= 0, row = 0) # pack() is used to show the label on the screen
+
+# Button
+new_button = Button(text="Click Me") # button is an object
+new_button.grid(column = 2,  row = 0) # pack() is used to show the button on the screen
+
 button = Button(text="Click Me", command = button_clicked) # button is an object
-button.pack() # pack() is used to show the button on the screen
+button.grid(column = 1,  row = 1) # pack() is used to show the button on the screen
 
 
 # Entry
-
 input = Entry(width=10)
-input.pack() # pack() is used to show the entry on the screen
+input.grid(column = 3,  row = 2) # pack() is used to show the entry on the screen
  
 
 
