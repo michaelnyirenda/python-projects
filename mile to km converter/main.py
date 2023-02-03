cad327925a78116d7f6d2377d9ebcb66233c3dd0
Michael Nyirenda <michael00nyirenda@gmail.com>
2023-02-04 01:35:29 +0200

    small refinements

--- mile to km converter/main.py
+++ mile to km converter/main.py
@@ -12,24 +12,19 @@ window.config(padx=10, pady=10) # padding
 # Label
 miles = Label(text="Miles", font=("Arial"))
 miles.grid(column= 2, row = 0) # pack() is used to show the label on the screen
-miles.config(padx=10, pady=10)
 
 equal = Label(text="is equal to", font=("Arial"))
 equal.grid(column= 0, row = 1) # pack() is used to show the label on the screen
-equal.config(padx=10, pady=10)
 
 km = Label(text="Km", font=("Arial"))
 km.grid(column= 2, row = 1) # pack() is used to show the label on the screen
-km.config(padx=10, pady=10)
 
 result = Label(text="0", font=("Arial"))
 result.grid(column= 1, row = 1) # pack() is used to show the label on the screen
-result.config(padx=10, pady=10)
 
 # Button
 button = Button(text="Calculate", command = button_clicked) # button is an object
 button.grid(column = 1,  row = 2) # pack() is used to show the button on the screen
-button.config(padx=10, pady=10)
 
 # Entry
 input = Entry(width=10)
