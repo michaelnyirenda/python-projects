c4e78b844b7ec4a39e9526062c485a009d3935df
Michael Nyirenda <michael00nyirenda@gmail.com>
2023-02-03 23:06:34 +0200

    buttons, entry and setting component

--- mile to km converter/playground.py
+++ mile to km converter/playground.py
@@ -6,7 +6,25 @@ def add(*args):
 
 print(add(1,2,3))
 
-def calculate(**kwargs):
+def calculate(n, **kwargs):
+    print(kwargs)
+    # for key, value in kwargs.items():
+    #     print(key)
+    #     print(value)
+    n+=kwargs["add"]
+    n*=kwargs["multiply"]
+    print(n)
+
     
-    
-calculate(add=3, multiply=5)
+calculate(2, add=3, multiply=5)
+
+class Car:
+    def __init__(self, **kw):
+        self.make = kw.get("make")
+        self.model = kw.get("model")
+        self.colour = kw.get("colour")
+        self.seats = kw.get("seats")
+        
+my_car = Car(make="Nissan", model="GT-R")
+print(my_car.make)
+print(my_car.model)
--- mile to km converter/practice.py
+++ mile to km converter/practice.py
@@ -1,6 +1,6 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 
 window.title("My first GUI program")
 
@@ -8,17 +8,24 @@ window.minsize(width=500, height=300)
  
 # Label
 
-my_label = tkinter.Label(text="I am a label", font=("Arial", 24, "italic"))
-my_label.pack(side = "left") # pack() is used to show the label on the screen
+my_label = Label(text="I am a label", font=("Arial", 24, "italic"))
+my_label.pack() # pack() is used to show the label on the screen
 
 
+# Button
+
+def button_clicked():
+    my_label.config(text=input.get()) # get() is used to get the text from the entry
+
+button = Button(text="Click Me", command = button_clicked) # button is an object
+button.pack() # pack() is used to show the button on the screen
 
 
+# Entry
 
-
-
-
-
+input = Entry(width=10)
+input.pack() # pack() is used to show the entry on the screen
+ 
 
 
 
