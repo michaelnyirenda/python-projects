737ae6ca803ecc614f38cd5dd4b63ba667eed520
Michael Nyirenda <michael00nyirenda@gmail.com>
2023-02-05 22:26:54 +0200

    added password manager logo

--- password manager/main.py
+++ password manager/main.py
@@ -1,5 +1,22 @@
+from tkinter import *
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+window.title("Password Generator")
+window.config(padx=20, pady=20)
+
+photo = PhotoImage(file="logo.png")
+
+canvas = Canvas(width=200, height=200)
+canvas.create_image(100, 100, image=photo)
+canvas.pack()
+
+
+
+
+window.mainloop()
