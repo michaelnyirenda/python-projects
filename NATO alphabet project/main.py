5ff3155e3be8e66513225a5db01abd2734c77c08
Michael Nyirenda <michael00nyirenda@gmail.com>
2023-02-08 19:08:43 +0200

    implementing error handling

--- NATO alphabet project/main.py
+++ NATO alphabet project/main.py
@@ -1,11 +1,17 @@
 import pandas as p
 
-#TODO 1. Create a dictionary in this format:
+
 # {"A": "Alfa", "B": "Bravo"}
 data = p.read_csv("nato_phonetic_alphabet.csv")
 nato_dict = {row.letter:row.code for (index, row) in data.iterrows()}
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-word = input("Enter a word: ").upper()
-nato_code_words = [nato_dict[letter] for letter in word]
+while True:
+    word = input("Enter a word: ").upper()
+    try: 
+        nato_code_words = [nato_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        continue
+    else:
+        break
 print(nato_code_words)
