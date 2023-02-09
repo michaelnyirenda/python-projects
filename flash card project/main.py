bbc63bbaf394b635a64f11d7a3c30b0c368592c2
Michael Nyirenda <michael00nyirenda@gmail.com>
2023-02-09 23:54:14 +0200

    UI setup

--- flash card project/main.py
+++ flash card project/main.py
@@ -1,2 +1,35 @@
 BACKGROUND_COLOR = "#B1DDC6"
+from tkinter import *
 
+
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Flashy")
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+
+canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
+card_front = PhotoImage(file="images/card_front.png")
+card_back = PhotoImage(file="images/card_back.png")
+
+canvas.create_image(400, 263, image=card_front)
+canvas.create_text(400, 150, text="Title", font=("Arial", 40, "italic"))
+canvas.create_text(400, 263, text="Word", font=("Arial", 60, "bold"))
+canvas.grid(column=0, row=0, columnspan=2)
+
+canvas.create_image(400, 263, image=card_back)
+canvas.create_text(400, 150, text="Title", font=("Arial", 40, "italic"))
+canvas.create_text(400, 263, text="Word", font=("Arial", 60, "bold"))
+canvas.grid(column=0, row=0, columnspan=2)
+
+
+# Buttons
+right = PhotoImage(file="images/right.png")
+right_button = Button(image=right, highlightthickness=0)
+right_button.grid(column=1, row=1)
+
+wrong = PhotoImage(file="images/wrong.png")
+wrong_button = Button(image=wrong, highlightthickness=0)
+wrong_button.grid(column=0, row=1)
+
+window.mainloop()
