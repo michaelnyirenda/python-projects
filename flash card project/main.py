906f21c06e76c200380f013514cdf9f892a7f426
Michael Nyirenda <michael00nyirenda@gmail.com>
2023-02-15 00:02:10 +0200

    reworking code

--- flash card project/main.py
+++ flash card project/main.py
@@ -31,21 +31,12 @@ window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
 canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
-
-# card front
 card_front = PhotoImage(file="images/card_front.png")
 card_back = PhotoImage(file="images/card_back.png")
 canvas_image = canvas.create_image(400, 263, image=card_front)
 title = canvas.create_text(400, 150, text="", font=("Arial", 40, "italic"))
 card_word = canvas.create_text(400, 263, text="", font=("Arial", 60, "bold"))
 canvas.grid(column=0, row=0, columnspan=2)
-
-
-# card back 
-# title_back = canvas.create_text(400, 150, text="", font=("Arial", 40, "italic"))
-# card_word_back = canvas.create_text(400, 263, text="", font=("Arial", 60, "bold"))
-# canvas.grid(column=0, row=0, columnspan=2)
-
 
 # Buttons
 correct = PhotoImage(file="images/right.png",)
