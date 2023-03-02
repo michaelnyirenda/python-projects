537403c325a0606b981249697ae9443aff50eebe
Michael Nyirenda <michael00nyirenda@gmail.com>
2023-03-02 16:02:20 +0200

    ISS Tracker complete

--- ISS Tracker/main.py
+++ ISS Tracker/main.py
@@ -1,37 +1,62 @@
 import requests
 from datetime import datetime
+import smtplib
+import time
 
-MY_LAT = 51.507351 # Your latitude
-MY_LONG = -0.127758 # Your longitude
+MY_LAT = -22.560881 # Your latitude
+MY_LONG = -17.065756 # Your longitude
+MY_EMAIL = "swankdotdev@gmail.com"
+MY_PASSWORD = "snbkyochxbroniqa"
 
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
-response.raise_for_status()
-data = response.json()
+def is_iss_overhead():
+    # Your position is within +5 or -5 degrees of the ISS position.
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    response.raise_for_status()
+    data = response.json()
 
-iss_latitude = float(data["iss_position"]["latitude"])
-iss_longitude = float(data["iss_position"]["longitude"])
+    iss_latitude = float(data["iss_position"]["latitude"])
+    iss_longitude = float(data["iss_position"]["longitude"])
 
-#Your position is within +5 or -5 degrees of the ISS position.
+    if -5 < abs(iss_latitude - MY_LAT) < 5 and -5 < abs(iss_longitude - MY_LONG) < 5:
+        return True
+
+def is_night():
+    parameters = {
+        "lat": MY_LAT,
+        "lng": MY_LONG,
+        "formatted": 0,
+    }
+
+    response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+    response.raise_for_status()
+    data = response.json()
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+
+    time_now = datetime.now()
+    if time_now.hour >= sunset or time_now.hour <= sunrise:
+        return True
+        # Then send me an email to tell me to look up.
+        
+while True:       
+    if is_iss_overhead() and is_night():
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(user=MY_EMAIL, password=MY_PASSWORD)
+        connection.sendmail(
+                from_addr=MY_EMAIL, 
+                to_addrs="boxmaster4427@gmail.com", 
+                msg="Subject:ISS TRACKER\n\ngo outside. the international space station is above you."
+                )
+    time.sleep(60)
 
 
-parameters = {
-    "lat": MY_LAT,
-    "lng": MY_LONG,
-    "formatted": 0,
-}
-
-response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
-response.raise_for_status()
-data = response.json()
-sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
-sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
-
-time_now = datetime.now()
-
-#If the ISS is close to my current position
-# and it is currently dark
-# Then send me an email to tell me to look up.
-# BONUS: run the code every 60 seconds.
 
 
 
+
+
+
+
+
+    
